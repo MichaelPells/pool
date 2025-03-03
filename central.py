3d1b3d05a218d2c4336db04ad0ab06b6ffa4f592
MichaelPells <themichaelpells@gmail.com>
2025-03-03 17:01:40 +0100

    Integrated a sample node

--- central.py
+++ central.py
@@ -1,3 +1,6 @@
+from pool import Pool, Task
+import threading
+
 class Network:
     def __init__(self, app):
         self.app = app
@@ -8,6 +11,9 @@ class Network:
 
         self.app(self)
 
+    def run(self, input):
+        self.inputs.append(input)
+
     def addstage(self, status, stage):
         if status not in self.stages:
             self.stages[status] = []
@@ -17,6 +23,9 @@ class Network:
     def start(self):
         self.running = True
 
+        threading.Thread(target=self.run)
+
+    def run(self):
         while self.running:
             if len(self.inputs) > 0:
                 status = self.inputs[0]["status"] if "status" in self.inputs[0] else "entrypoint"
@@ -29,10 +38,17 @@ class Network:
 
 def app(network):
     def main():
+        pool = Pool()
+        pool.start()
+
         for i in range(5):
             print(i)
 
-        # Branch into a node to do a blocking task with status 'after-looper'.
+        def blocker():
+            for i in range(10000): pass
+
+            # report status 'after-looper' to `network`
+        pool.assign(blocker)
 
         def post_looper_result(result):
             print(result)
